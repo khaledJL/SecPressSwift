c40211d68507192131c47e0184d56a059b852142
Apple <imac@imac-de-apple.local>
2019-04-24 11:43:52 +0100

    Initial Commit

---
+++ SecPress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecPress
+//
+//  Created by Apple on 4/24/19.
+//  Copyright © 2019 Anypli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
